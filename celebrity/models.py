9365a44ab95d0c31c69213f4a19869791b146d65
Dustin Keitel <dustin.keitel@gmail.com>
2014-08-18 13:30:44 -0400

    Defaults for whitelist and flagged, ability to add people

--- celebrity/models.py
+++ celebrity/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.forms import ModelForm
 
 class Celebrity(models.Model):
     def __unicode__(self):
         return self.name
         
-    name = models.CharField(max_length=300)
-    flagged = models.BooleanField()
-    whitelisted = models.BooleanField()
+    name = models.CharField(max_length=300, help_text=None)
+    flagged = models.BooleanField(default=False)
+    whitelisted = models.BooleanField(default=False)
     #add_date = models.DateTimeField('date added')
     #contributor = models.ForeignKey('auth.User')
 
     class Meta:
         verbose_name_plural = 'celebrities'
+
+class CelebrityForm(ModelForm):
+    class Meta:
+        model = Celebrity
+        fields = ('name',)
+        help_text = ""
--- celebrity/views.py
+++ celebrity/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
+from django.forms.models import modelformset_factory
 from django.template import RequestContext
 
-from celebrity.models import Celebrity
+from celebrity.models import Celebrity, CelebrityForm
 from penvelopekravitz.settings import CELEBS_TO_SEND, ROUND_TIME
 
 import random
@@ -27,4 +29,18 @@ def play(request):
     return render_to_response('play.html', {'celebs':names, 'max_celebs' : CELEBS_TO_SEND, 'max_time' : ROUND_TIME}, context_instance=RequestContext(request))
 
 def add_celeb(request):
-    return render_to_response('add.html', context_instance=RequestContext(request))
+    cf = modelformset_factory(Celebrity, CelebrityForm, max_num=10, extra=10)
+    if request.method == 'POST':
+        formset = cf(request.POST)
+        if formset.is_valid():
+            formset.save()
+            return HttpResponseRedirect(reverse('add_celeb'))
+        else:
+            render_to_response('add.html', 
+            {"formset": formset,
+            'error_message': "Invalid celebrity name"},
+            context_instance=RequestContext(request))
+            
+    else:
+        formset = cf(queryset=Celebrity.objects.none())
+    return render_to_response('add.html', {"formset": formset}, context_instance=RequestContext(request))
